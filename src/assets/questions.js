18a07d38d15a82b1f976cdea6257d4299c575c41
Mai Đoàn <doanngocmai2000@gmail.com>
2022-08-18 14:32:41 +0700

    add data assets/questions.js

---
+++ src/assets/questions.js
@@ -0,0 +1,124 @@
+const questions = [
+  {
+    id: 1,
+    questionText:
+      "Rolex is a company that specializes in what type of product?",
+    image:
+      "https://media.ex-cdn.com/EXP/media.watchbook.vn/files/anhi/2020/01/11/ahmed-seddiqi-and-sons-rolex-dubai-watch-week-1-1376x1032-1258.jpg",
+    answerOptions: [
+      {
+        answerText: "Phone",
+        isCorrect: false,
+      },
+      {
+        answerText: "Watches",
+        isCorrect: true,
+      },
+      {
+        answerText: "Food",
+        isCorrect: false,
+      },
+      {
+        answerText: "Cosmetic",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    questionText: "When did the website `Facebook` launch?",
+    image:
+      "https://vnn-imgs-f.vgcloud.vn/2022/03/25/07/facebook-ho-tro-quang-cao-3d-instagram-tien-toi-metaverse.jpg",
+    answerOptions: [
+      {
+        answerText: "2004",
+        isCorrect: true,
+      },
+      {
+        answerText: "2005",
+        isCorrect: false,
+      },
+      {
+        answerText: "2006",
+        isCorrect: false,
+      },
+      {
+        answerText: "2007",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    questionText: "Who played the character of harry potter in movie?",
+    image: "https://picfiles.alphacoders.com/622/62263.jpg",
+    answerOptions: [
+      {
+        answerText: "Johnny Deep",
+        isCorrect: false,
+      },
+      {
+        answerText: "Leonardo Di Caprio",
+        isCorrect: false,
+      },
+      {
+        answerText: "Denzel Washington",
+        isCorrect: false,
+      },
+      {
+        answerText: "Daniel Red Cliff",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    questionText: "What is the capital of France?",
+    image:
+      "https://deviet.vn/wp-content/uploads/nhung-dia-diem-du-lich-phap.jpg",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Paris", isCorrect: true },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    questionText: "Who is CEO of Tesla?",
+    image:
+      "https://images.baoquangnam.vn/Storage/NewsPortal/2020/9/24/93203/Tesla-1.jpg",
+    answerOptions: [
+      { answerText: "Jeff Bezos", isCorrect: false },
+      { answerText: "Elon Musk", isCorrect: true },
+      { answerText: "Bill Gates", isCorrect: false },
+      { answerText: "Tony Stark", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    questionText: "The iPhone was created by which company?",
+    image:
+      "https://photo-cms-viettimes.zadn.vn/666x374/Uploaded/2022/cpuotbp/2017_09_14/applethanhlapcongtytaivietnamrvws_gjuj.jpg",
+    answerOptions: [
+      { answerText: "Apple", isCorrect: true },
+      { answerText: "Intel", isCorrect: false },
+      { answerText: "Amazon", isCorrect: false },
+      { answerText: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    id: 7,
+    questionText: "How many Harry Potter books are there?",
+    image:
+      "https://upload.wikimedia.org/wikipedia/vi/d/db/Harry-potter-films.jpg",
+    answerOptions: [
+      { answerText: "1", isCorrect: false },
+      { answerText: "4", isCorrect: false },
+      { answerText: "6", isCorrect: false },
+      { answerText: "7", isCorrect: true },
+    ],
+  },
+];
+
+export default questions;
--- src/views/app/JoinContest/JoinContest.tsx
+++ src/views/app/JoinContest/JoinContest.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import '../../../styles/quiz.scss'
+import questions from "../../../assets/questions"
+
 
 export const JoinContest = () => {
-  return (
-    <>
-      Join Contest
-    </>
-  )
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const handleAnswerOptionClick = (isCorrect: boolean) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
+    return (
+        <div className='show-quiz'>
+            {showScore ? (
+                <div className='score-section'>
+                    You scored {score} out of {questions.length}
+                </div>
+            ) : (
+                <div>
+                    <div className='question-section'>
+                        <div className='question-count'>
+                            <span>Question {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+
+                        <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                        <div className='question-img'><img src={questions[currentQuestion].image} alt={""}/></div>
+
+                    </div>
+                    <div className='answer-section'>
+                        {questions[currentQuestion].answerOptions.map((answerOption: { isCorrect: boolean; answerText: string }, index) => (
+                            <button key={index}
+                                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
